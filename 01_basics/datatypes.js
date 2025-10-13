5950452f5b7daa40584084eec28bd220d51ff53a
Rohan Rai <iamrohanrai@gmail.com>
2025-10-13 11:40:52 +0530

    added datatypes file

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); alert available in browser not in node js
+
+// code readability should be the first priority
+
+// PRIMITIVE DATATYPES
+let name = "rohan"; // string
+let age = 18; // number
+let isLoggedIn = false; // boolean datatype
+let value = null; // null (standalone value);
+let state; // undefined
+// symbol => used for uniqueness
+
+// number range => 2 to power 53
+
+// NON-PRIMITVE DATATYPE
+let information = {}; // object
+let arr = []; // array
+
+console.log(typeof null); // object //it was a bug
+console.log(typeof undefined); // undefined
