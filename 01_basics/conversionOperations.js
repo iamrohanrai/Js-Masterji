ca1cafd36b37e808e1956d839313b935691ec8f3
Rohan Rai <iamrohanrai@gmail.com>
2025-10-14 11:46:36 +0530

    added conversionOperation

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,31 @@
+let score = null;
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1;
+// false = 0;
+// null = 0;
+// undefined = NaN
+
+let isLoggedIn = 1;
+console.log(typeof isLoggedIn);
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+console.log(typeof booleanisLoggedIn);
+
+// "" = false;
+// "rohan" = true
+// 1 = true
+// 0 = false
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
