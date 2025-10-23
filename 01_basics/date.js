ea8303e0cdc94f6b1669fdb496f9a90664520757
Rohan Rai <iamrohanrai@gmail.com>
2025-10-23 12:28:48 +0530

    added date file

---
+++ 01_basics/date.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2025, 1, 23); // month start from 0 in js
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamps = Date.now();
+console.log(myTimeStamps);
