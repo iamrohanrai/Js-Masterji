f55802ced1b6d797dcbb233d26f88daa4b05db6c
Rohan Rai <iamrohanrai@gmail.com>
2025-11-02 12:05:30 +0530

    learned about object destructuring

---
+++ 02_basics/objects_05.js
@@ -0,0 +1,14 @@
+const course = {
+  courseName: "js",
+  price: "999",
+  courseInstructor: "rohan",
+};
+// course.courseInstructor
+const { courseInstructor } = course;
+console.log(courseInstructor);
+
+// const { courseInstructor: instructor } = course;
+// console.log(instructor);
+
+
+const navbar
