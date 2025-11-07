d07a99abbeca41c1cc9a82177a9eb3dc47f6b678
Rohan Rai <iamrohanrai@gmail.com>
2025-11-07 12:00:46 +0530

    added while and do while file

---
+++ 05_loops/two.js
@@ -0,0 +1,19 @@
+// let index = 1;
+// while (index <= 5) {
+//   console.log(`value of index is ${index}`);
+//   index++;
+// }
+
+let myArray = ["flash", "batman", "superman"];
+let arr = 0;
+// while (arr < myArray.length) {
+//   console.log(`value is ${myArray[arr]}`);
+//   arr++;
+// }
+
+let score = 11;
+
+do {
+  console.log(`Score is ${score}`);
+  score = score + 1;
+} while (score <= 10);
